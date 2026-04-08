7e5a0644fb20231dc306260c821262dc84e89be9
Wellington <nylook2@hotmail.com>
2026-04-08 18:42:16 -0300

    :zap: Corrigindo para melhorar

--- TarefasAPI/Program.cs
+++ TarefasAPI/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using TarefasAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -12,5 +16,18 @@ builder.Services.AddCors(options =>
               .AllowAnyMethod());
 });
 
-// E no pipeline (depois do builder.Build()):
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
 app.UseCors("AllowAngular");
+app.MapControllers();
+
+app.Run();
